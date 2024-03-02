afde2034cd30c2a05e368b4eedff13e0131dfcc6
juzybits <96890594+juzybits@users.noreply.github.com>
2024-03-02 22:48:56 -0100

    basic zkSend

--- web/src/PageHome.tsx
+++ web/src/PageHome.tsx
@@ -1,9 +1,49 @@
+import { useCurrentAccount, useSignTransactionBlock, useSuiClient } from '@mysten/dapp-kit';
+import { ZkSendLinkBuilder } from '@mysten/zksend';
+
 export const PageHome: React.FC = () =>
 {
+    const currAcct = useCurrentAccount();
+    const suiClient = useSuiClient();
+    const { mutateAsync: signTransactionBlock } = useSignTransactionBlock();
+
+    const send = async () => {
+        if (!currAcct) {
+            return;
+        }
+        const link = new ZkSendLinkBuilder({
+            sender: currAcct.address,
+        });
+
+        // SUI
+        // link.addClaimableMist(BigInt(1));
+
+        // FUD
+        const fudCoinType = '0x76cb819b01abed502bee8a702b4c2d547532c12f25001c9dea795a5e631c26f1::fud::FUD';
+        // link.addClaimableBalance(fudCoinType, BigInt(100_000));
+        link.addClaimableBalance(fudCoinType, BigInt(1));
+
+        const url = link.getLink();
+        console.log('url: ', url);
+
+        const txb = await link.createSendTransaction();
+        const signedTxb = await signTransactionBlock({
+            transactionBlock: txb,
+        });
+        const resp = await suiClient.executeTransactionBlock({
+            transactionBlock: signedTxb.transactionBlockBytes,
+            signature: signedTxb.signature,
+            options: { showEffects: true },
+        });
+        console.log('resp:', resp);
+    };
+
     return <div id='page-home' className='page'>
-        <h1>Home</h1>
+        <h1>zkSend any Sui coin</h1>
         <div className='content'>
-            LFG
+            <div>
+                {currAcct && <button onClick={() => { send() }}>Send</button>}
+            </div>
         </div>
     </div>;
 }
