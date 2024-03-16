7fd303e3e0f1b08b274815694a357e5f3e8ec5cf
juzybits <96890594+juzybits@users.noreply.github.com>
2024-03-16 20:17:23 -0100

    styles

--- web/src/PageHome.tsx
+++ web/src/PageHome.tsx
@@ -4,22 +4,22 @@ export const PageHome: React.FC = () =>
         <h1>Home</h1>
         <h2>Send Sui assets simply by sharing a link</h2>
 
+        <br/>
         <h3>How it works, at a high level:</h3>
         <p>⤴️ The sender adds any coins or assets to a zkSend link, then shares the link with the recipient via email / DM / etc.</p>
 
         <p>⤵️ The recipient visits the link and clicks a button to claim the assets, without signing a transaction, or even connecting their wallet, in a non-custodial manner.</p>
 
-        <h3>What the app does under the hood:</h3>
+        <br/>
+        <h3>How it works, under the hood:</h3>
 
-        <p>⤴️ The sender:</p>
-        <p>
+        <p>⤴️ The sender:<br/>
             - Creates a one-off Sui keypair for the zkSend link.<br/>
             - Sends the assets to the keypair's public address, plus a tiny bit of SUI to pay for the claim txn fees.<br/>
             - Generates a link back to the webapp such that the URL includes the keypair's private key.
         </p>
 
-        <p>⤵️ The recipient:</p>
-        <p>
+        <p>⤵️ The recipient:<br/>
             - Reconstructs the one-off keypair from the private key found in the URL.<br/>
             - Looks for assets under that public address, and shows them to the user.<br/>
             - Sends the assets to an address provided by the user by executing a txn from the keypair.<br/>
