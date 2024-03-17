ac6e68e178be4d2975ff578f0c609e3fa4f73474
juzybits <96890594+juzybits@users.noreply.github.com>
2024-03-16 23:37:12 -0100

    claim: support multiple coin types

--- web/src/PageClaim.tsx
+++ web/src/PageClaim.tsx
@@ -3,10 +3,11 @@ import { formatBigInt, shortenSuiAddress, validateAndNormalizeSuiAddress } from 
 import { useEffect, useState } from 'react';
 import { useLocation, useOutletContext } from 'react-router-dom';
 import { AppContext } from './App';
-import { coinInfo } from './constants';
 import { ZkSendLink } from './lib/zksend';
+import { CoinInfo, getCoinInfo } from './utils';
 
 type ListClaimableAssetsReturnType = Awaited<ReturnType<InstanceType<typeof ZkSendLink>['listClaimableAssets']>>;
+type BalancesType = ListClaimableAssetsReturnType['balances'];
 
 export const PageClaim: React.FC = () =>
 {
@@ -17,57 +18,53 @@ export const PageClaim: React.FC = () =>
     const suiClient = useSuiClient();
     const { mutate: disconnect } = useDisconnectWallet();
 
+    const [ link, setLink ] = useState<ZkSendLink>();
     const [ claimableAssets, setClaimableAssets ] = useState<ListClaimableAssetsReturnType>();
+    const [ claimableCoinsInfo, setClaimableCoinsInfo ] = useState<CoinInfo[]>();
     const [ inputAddress, setInputAddress ] = useState('');
     const [ claimTxnDigest, setClaimTxnDigest ] = useState<string>();
 
     const { inProgress, setInProgress, openConnectModal } = useOutletContext<AppContext>();
     const [ errMsg, setErrMsg ] = useState('');
 
-    const claimAddress = currAcct ? currAcct.address : inputAddress;
-    const cleanAddress = validateAndNormalizeSuiAddress(claimAddress);
-    const shortAddress = shortenSuiAddress(cleanAddress);
-
-    const claimableBalance = !claimableAssets ? BigInt(0)
-    : claimableAssets.balances.reduce((total, balance) => {
-        return balance.coinType !== coinInfo.coinType
-            ? BigInt(0)
-            : total + balance.amount;
-    }, BigInt(0));
-    const claimableBalancePretty = formatBigInt(claimableBalance, coinInfo.decimals, 'compact');
-
     useEffect(() => {
-        loadClaimableAssets();
+        loadZkSendLink();
     }, []);
 
-    const getZkSendLink = async () => {
-        return await ZkSendLink.fromUrl(window.location.href, {
+    const loadZkSendLink = async () => {
+        const link = await ZkSendLink.fromUrl(window.location.href, {
             client: suiClient,
         });
-    }
+        setLink(link);
+        loadClaimableAssets(link)
+    };
 
-    const loadClaimableAssets = async () => {
+    const loadClaimableAssets = async (link: ZkSendLink) => {
         try {
-            const link = await getZkSendLink();
             const assets = await link.listClaimableAssets('0x123'); // dummy address, doesn't matter
             console.debug('assets:', assets);
             setClaimableAssets(assets);
+            loadClaimableCoinsInfo(assets.balances);
         } catch(err) {
             setErrMsg(String(err));
         }
     };
 
-    const claimAssets = async () => {
+    const loadClaimableCoinsInfo = async (balances: BalancesType) => {
+        const newClaimableCoinsInfo: CoinInfo[] = [];
+        for (const balance of balances) {
+            const coinInfo = await getCoinInfo(balance.coinType, suiClient);
+            newClaimableCoinsInfo.push(coinInfo);
+        }
+        setClaimableCoinsInfo(newClaimableCoinsInfo);
+    }
+
+    const claimAssets = async (link: ZkSendLink, recipientAddress: string) => {
         setErrMsg('');
-        if (!cleanAddress) return;
-
         setInProgress(true);
-        document.body.style.cursor = 'wait';
         try {
-            const link = await getZkSendLink();
-            const resp = await link.claimAssets(cleanAddress);
+            const resp = await link.claimAssets(recipientAddress);
             console.debug('resp:', resp);
-
             if (resp.errors) {
                 setErrMsg(`Txn digest: ${resp.digest}\nTxn errors: ${JSON.stringify(resp.errors)}`);
             } else {
@@ -78,7 +75,6 @@ export const PageClaim: React.FC = () =>
             setErrMsg(String(err));
         } finally {
             setInProgress(false);
-            document.body.style.cursor = 'default';
         }
     };
 
@@ -114,7 +110,7 @@ export const PageClaim: React.FC = () =>
             return <div className='success-box'>
                 <h1>Success</h1>
 
-                <p>Copy and share the link with the person you want to ${coinInfo.symbol}.</p>
+                <p>Copy and share the link with the person you want to send the assets to.</p>
 
                 <button className='btn' onClick={copyLink}>
                     COPY LINK
@@ -127,33 +123,39 @@ export const PageClaim: React.FC = () =>
                 </p>
 
                 <p>
-                        We don't store claim links. If you don't share or save your link before leaving this page, the ${coinInfo.symbol} will be lost.
+                    We don't store claim links. If you don't share or save your link before leaving this page, the assets will be lost.
                 </p>
             </div>;
         }
+
+        const claimAddress = currAcct ? currAcct.address : inputAddress;
+        const normalizedAddress = validateAndNormalizeSuiAddress(claimAddress);
+        const shortAddress = shortenSuiAddress(normalizedAddress);
 
         // claim was successful
         if (claimTxnDigest) {
             return <div className='success-box'>
                 <h1>Success</h1>
-                <p>{claimableBalancePretty} ${coinInfo.symbol} was sent to <span style={{whiteSpace: 'nowrap'}}>{shortAddress}</span></p>
+                <p>Assets were sent to <span style={{whiteSpace: 'nowrap'}}>{shortAddress}</span></p>
             </div>;
         }
 
         // assets are loading
-        if (!claimableAssets) {
+        if (!link || !claimableAssets || !claimableCoinsInfo) {
             if (errMsg) {
                 return null; // something went wrong on load
             }
             return <h1>Loading...</h1>;
         }
 
+        const claimableBalances = claimableAssets.balances.filter(bal => bal.amount > 0);
+
         // already claimed
-        if (claimableBalance === BigInt(0)) {
+        if (claimableBalances.length === 0) {
             return <>
-                <h1>${coinInfo.symbol} has already been claimed</h1>
+                <h1>Assets have already been claimed</h1>
                 <div>
-                    <p>The ${coinInfo.symbol} in this link are no longer available.</p>
+                    <p>The coins in this link are no longer available.</p>
                 </div>
             </>;
         }
@@ -161,12 +163,25 @@ export const PageClaim: React.FC = () =>
         // assets are ready to claim
         return <>
             <h1>
-                Found {claimableBalancePretty} ${coinInfo.symbol}
+                Found assets
             </h1>
+
+            <div>
+            {claimableBalances.map(bal => {
+                const coinInfo = claimableCoinsInfo.find(info => info.coinType === bal.coinType);
+                if (!coinInfo) {
+                    throw new Error('TODO this should never happen');
+                }
+                const claimableBalancePretty = formatBigInt(bal.amount, coinInfo.decimals, 'compact');
+                return <div key={bal.coinType}>
+                    {claimableBalancePretty} {coinInfo.symbol}
+                </div>
+            })}
+            </div>
 
             {!currAcct &&
             <>
-                <p>Enter the address where you want to send the ${coinInfo.symbol},<br/>or&nbsp;
+                <p>Enter the address where you want to send the assets,<br/>or&nbsp;
                     <button className='btn' onClick={openConnectModal}>LOG IN</button>
                     &nbsp;with your wallet to auto-fill.
                 </p>
@@ -178,14 +193,14 @@ export const PageClaim: React.FC = () =>
                 />
             </>}
 
-            {cleanAddress &&
+            {normalizedAddress &&
             <p>
                 Recipient: {shortAddress}
             </p>}
 
-            {cleanAddress &&
-            <button className='btn' onClick={claimAssets} disabled={inProgress}>
-                CLAIM ${coinInfo.symbol}
+            {normalizedAddress &&
+            <button className='btn' onClick={() => { claimAssets(link, normalizedAddress) }} disabled={inProgress}>
+                CLAIM ASSETS
             </button>}
 
             {currAcct &&
