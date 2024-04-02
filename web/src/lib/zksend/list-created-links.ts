cf03bf145b2cde8d4a8d08cbbc3dcbeb6e1db480
juzybits <96890594+juzybits@users.noreply.github.com>
2024-04-02 20:11:33 +0000

    fetch.bind(window) on zksend/list-created-links.ts for SuiGraphQLClient

--- web/src/lib/zksend/list-created-links.ts
+++ web/src/lib/zksend/list-created-links.ts
@@ -85,6 +85,7 @@ export async function listCreatedLinks({
 			network === 'testnet'
 				? 'https://sui-testnet.mystenlabs.com/graphql'
 				: 'https://sui-mainnet.mystenlabs.com/graphql',
+		fetch: fetch.bind(window),
 	});
 
 	const packageId = normalizeSuiAddress(contract.packageId);
