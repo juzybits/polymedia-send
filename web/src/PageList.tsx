b32c5ee4b7e6421192fcde8ed62f95840404954a
juzybits <96890594+juzybits@users.noreply.github.com>
2024-04-02 20:39:00 +0000

    list: list the links

--- web/src/PageList.tsx
+++ web/src/PageList.tsx
@@ -1,27 +1,43 @@
 import { useCurrentAccount } from '@mysten/dapp-kit';
+import { useEffect, useState } from 'react';
 import { listCreatedLinks } from './lib/zksend/list-created-links';
-import { useEffect } from 'react';
 
 export const PageList: React.FC = () =>
 {
+    const [ links, setLinks ] = useState<Awaited<ReturnType<typeof listCreatedLinks>>>();
     const currAcct = useCurrentAccount();
+
     useEffect(() => {
-        if (currAcct) {
-            (async () => {
-                const { links, /*hasNextPage, cursor*/ } = await listCreatedLinks({
+        const loadLinks = async () => {
+            if (!currAcct) {
+                setLinks(undefined);
+            } else {
+                const res = await listCreatedLinks({
                     address: currAcct.address,
                 });
-                console.log("=== LINKS ===");
-                for (const link of links) {
-                    // const { nfts, balances } = await links[0].assets;
-                    console.log(link);
-                }
-            })();
-        }
+                setLinks(res);
+            }
+        };
+        loadLinks();
     }, [currAcct]);
 
     return <div id='page-list'>
         <h1>Your links</h1>
-        <p>Soon...</p>
+        {
+            !links
+            ?
+                <p>Loading...</p>
+            :
+            links.links.map(link =>
+                <div key={link.digest} className='tight'>
+                    <p>claimed: {link.claimed ? 'yes' : 'no'}</p>
+                    <p>createdAt: {link.createdAt}</p>
+                    <p>digest: {link.digest}</p>
+                    <p>address: {link.link.address}</p>
+                    {link.assets.balances.map(bal =>
+                        <p>Balance: {String(bal.amount)} {bal.coinType.split('::')[2]}</p>
+                    )}
+                </div>)
+            }
     </div>;
 }
