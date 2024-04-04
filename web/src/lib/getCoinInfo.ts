355cde0115c83e0650d3ff44c332799c79a42b3a
juzybits <96890594+juzybits@users.noreply.github.com>
2024-04-04 14:26:39 +0000

    getCoinInfo: cache in a Map<string, CoinInfo>

--- web/src/lib/getCoinInfo.ts
+++ web/src/lib/getCoinInfo.ts
@@ -10,39 +10,42 @@ export type CoinInfo = {
     iconUrl: string|null,
 };
 
-const coinInfoCache: CoinInfo[] = [
+const initialCoinInfo: CoinInfo[] = [
     // mainnet
-    {
+    {   symbol: 'SUI', decimals: 9, iconUrl: null,
         coinType: '0x2::sui::SUI',
-        symbol: 'SUI', decimals: 9, iconUrl: null,
     },
-    {
+    {   symbol: 'FUD', decimals: 5, iconUrl: null,
         coinType: '0x76cb819b01abed502bee8a702b4c2d547532c12f25001c9dea795a5e631c26f1::fud::FUD',
-        symbol: 'FUD', decimals: 5, iconUrl: null,
     },
     // testnet
-    {
+    {   symbol: 'FUD', decimals: 5, iconUrl: null,
         coinType: '0x1fe2bdb8d9dba5bb2f8f1d987fcb9ab53d0f38b8a42445ebed736d6708ca59d6::fud::FUD',
-        symbol: 'FUD', decimals: 5, iconUrl: null,
     },
 ];
 
+const coinInfoCache = new Map<string, CoinInfo>(
+    initialCoinInfo.map(info => [info.coinType, info])
+);
+
 export async function getCoinInfo(coinType: string, client: SuiClient): Promise<CoinInfo> {
-    for (const cachedInfo of coinInfoCache) {
-        if (cachedInfo.coinType === coinType) {
-            return cachedInfo;
-        }
+    const cachedInfo = coinInfoCache.get(coinType);
+    if (cachedInfo) {
+        return cachedInfo;
     }
-    const coinMeta = await client.getCoinMetadata({coinType});
+
+    const coinMeta = await client.getCoinMetadata({ coinType });
     if (!coinMeta) {
         throw new Error(`CoinMetadata not found for type: ${coinType}`);
     }
+
     const coinInfo: CoinInfo = {
         coinType,
         symbol: coinMeta.symbol,
         decimals: coinMeta.decimals,
         iconUrl: coinMeta.iconUrl ?? null,
     };
-    coinInfoCache.push(coinInfo);
+    coinInfoCache.set(coinType, coinInfo);
+
     return coinInfo;
 }
