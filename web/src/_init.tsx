8cf36f4f60b0bf6d17d4958428f776f95fd9596f
juzybits <96890594+juzybits@users.noreply.github.com>
2024-04-05 12:32:36 +0000

    BigInt.prototype.toJSON = function() { return this.toString() }

--- web/src/_init.tsx
+++ web/src/_init.tsx
@@ -1,6 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import { AppWrapRouter } from './App';
 
+// @ts-expect-error Property 'toJSON' does not exist on type 'BigInt'
+BigInt.prototype.toJSON = function() { return this.toString(); };
+
 ReactDOM
     .createRoot( document.getElementById('app') as Element )
     .render(<AppWrapRouter />);
